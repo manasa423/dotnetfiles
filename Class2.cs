c91bab0d08a0a4fc86734155694b2f0311b24576
manasa <54406519+manasa423@users.noreply.github.com>
2021-02-06 10:43:23 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Exercise32
+{
+    public static void Main()
+    {
+
+        int num, i, ctr = 0;
+
+   
+
+        Console.Write("Input  a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        for (i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                ctr++;
+                break;
+            }
+        }
+        if (ctr == 0 && num != 1)
+            Console.Write("{0} is a prime number.\n", num);
+        else
+            Console.Write("{0} is not a prime number\n", num);
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,27 @@
+﻿//factors of anumber
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Class3
+    {
+        static void Main(string[] args)
+        {
+            int num = 12;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine($"{i} are the factors");
+                }
+
+
+
+            }
+        }
+    }
+}
